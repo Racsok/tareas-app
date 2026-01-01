1164a52d0f18c4cfa42e19d309a34a3ec4280166
Racsok <oscarmcg02@gmail.com>
2026-01-01 13:32:19 -0500

    implementacion de la interfaz con la funciones de crear y mostrar tareas (ver el commit anterior)

--- proyecto/tkinter-tareas-app/src/main.py
+++ proyecto/tkinter-tareas-app/src/main.py
@@ -1,10 +1,7 @@
 #interfaz grafica
-# from tkinter import Tk
-# from tkinter import ttk
 from control.GestorTareas import GestorTareas
 from vista import *
 from vista.ventana_principal import Ventana
-from vista import ventana_crear_tarea   
 
 def main():
     control = GestorTareas()
